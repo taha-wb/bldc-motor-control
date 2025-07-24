c5f45e4c23c65d1c4b15fd1910fa1215177aa3a2
taha <saed.taha02@gmail.com>
2025-07-24 10:41:30 +0300

    battery temp and motor RPM handling logic

--- src/can_simulator.c
+++ src/can_simulator.c
@@ -24,9 +24,55 @@ void main(){
     CAN_Message socMsg = { .id = 0x101, .dlc = 2, .data = {75, 0} }; // 75% SOC
     CAN_Message batteryPackStatus = {.id = 	0x180 ,.dlc = 4, .data = {0x50, 0x01, 0x00, 0x00}};  // Battery Pack Info Message
 
+
+    // battery temp logic
+    CAN_Message batteryTemp = {.id = 0x180 ,.dlc = 8, .data =  {0x01, 0x90, 0, 0, 0, 0, 0, 0}};
+
+    uint16_t raw_temp = (batteryTemp.data[0] << 8) | batteryTemp.data[1] ;
+    float battery_temp = ((float)raw_temp) /10.0;
+
+
+    // validate & respond
+
+    
+    if (battery_temp > 60.0) {
+    // 🔴 Over-temperature
+    triggerCoolingSystem();
+    logEvent("Battery temp high");
+    } else if (battery_temp < 0.0) {
+    // 🟡 Too cold
+    restrictCharging();
+    logEvent("Battery temp low");
+    } else {
+    // ✅ Normal
+    updateDisplay(battery_temp);
+    }
+
+
+
+    // motor RPM logic 
+    CAN_Message motorRpm = {.id = 0x220 ,.dlc = 8, .data = {0x13, 0x88, 0, 0, 0, 0, 0, 0}};
+    uint16_t raw_rpm = (motorRpm.data[0] << 8) | motorRpm.data[1];
+    int motor_rpm = (int) raw_rpm;
+
+
+    // validate & respond
+
+    if (motor_rpm > 10000) {
+    logEvent("Over-speed detected");
+    enterProtectionMode();  // cut power, flag warning
+    } else if (motor_rpm < 0) {
+    logEvent("Invalid RPM value");  // should never happen with unsigned
+    } else {
+    updateDisplayRPM(motor_rpm);
+    }
+
+
+
     
     printCAN(socMsg);
     printCAN(batteryPackStatus);
+   
 
     
 }
